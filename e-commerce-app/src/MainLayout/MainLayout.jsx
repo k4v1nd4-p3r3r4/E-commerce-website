9227e4bd9ed98d727f95e53a7d3d53c7dbba41cc
Kavinda Perera <kavinda_2020675@fot.ruh.ac.lk>
2025-03-25 17:22:07 +0530

    project setup

---
+++ e-commerce-app/src/MainLayout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayout = () => {
+    return (
+        <div className='text-red-500'>
+            hello
+        </div>
+    );
+};
+
+export default MainLayout;
